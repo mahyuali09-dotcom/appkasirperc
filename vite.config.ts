a2413a78e0de7c7d03e92f2aaa86890a500484b2
Mahyu Ali <mahyuddin@email.com>
2026-04-19 20:13:39 +0800

    fix: add webpack-cli

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       },
-	  base: process.env.NODE_ENV === 'production' ? '/aplikasikasirperc/' : '/',
+	  base: process.env.NODE_ENV === 'production' ? '/appkasirperc/' : '/',
     };
 });
